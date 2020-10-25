29c789457e1a41a403340f4829d42c9ec62d7161
Jeff Chen <jchen@ladderlife.com>
2020-10-25 16:43:05 -0600

    self-host dangerously-set-html-content

--- components/BlogPost.jsx
+++ components/BlogPost.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import Link from "next/link";
 import Head from "next/head";
-import InnerHTML from "dangerously-set-html-content";
 
 import { Colors } from "../lib/constants";
 import { BlogContainer } from "../components/containers";
@@ -66,6 +65,22 @@ function formatDate(date) {
   const suffix = { 1: "st", 2: "nd", 3: "rd" }[day % 10] || "th";
 
   return `${month} ${day}${suffix}, ${year}`;
+}
+
+// https://github.com/christo-pr/dangerously-set-html-content/blob/master/src/index.js
+function InnerHTML(props) {
+  const { html, ...rest } = props;
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    if (!html) return;
+
+    const slotHtml = document.createRange().createContextualFragment(html); // Create a 'tiny' document and parse the html string
+    divRef.current.innerHTML = ""; // Clear the container
+    divRef.current.appendChild(slotHtml); // Append the new content
+  }, [html]);
+
+  return <div {...rest} ref={divRef}></div>;
 }
 
 export default function BlogPost({ post, opts = {} }) {
