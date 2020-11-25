5682595eab3ffefa0b5b8b92f88cfbacd3a37a47
Jeff Chen <hello@jeff.yt>
2020-11-24 19:29:11 -0800

    Update RootContainer.jsx

--- components/containers/RootContainer.jsx
+++ components/containers/RootContainer.jsx
@@ -1,4 +1,4 @@
-import styles from "styles/components/MainContainer.module.scss";
+import styles from "styles/components/RootContainer.module.scss";
 
 export default function RootContainer({ children }) {
   return <div className={styles.container}>{children}</div>;
