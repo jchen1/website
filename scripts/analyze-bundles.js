44c9f5e3e79422e59b2017e0629520760c64f72b
Jeff Chen <jchen@ladderlife.com>
2020-11-30 05:10:14 -0800

    will it run on push

--- scripts/analyze-bundles.js
+++ scripts/analyze-bundles.js
@@ -7,44 +7,49 @@ const bundle = require("../.next/build-manifest.json");
 const prefix = ".next";
 const outdir = path.join(process.cwd(), prefix, "analyze");
 const outfile = path.join(outdir, "bundle.txt");
+const outfileJSON = path.join(outdir, "bundle.json");
 
 function formatBytes(bytes, decimals = 2) {
-    if (bytes === 0) return '0 Bytes';
+  if (bytes === 0) return "0 Bytes";
 
-    const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 }
 
 const pageSizes = Object.keys(bundle.pages).map(p => {
-    const files = bundle.pages[p];
-    const size = files.map(filename => {
-        const fn = path.join(process.cwd(), prefix, filename);
-        const bytes = fs.readFileSync(fn);
-        const gzipped = zlib.gzipSync(bytes);
-        return gzipped.byteLength;
+  const files = bundle.pages[p];
+  const size = files
+    .map(filename => {
+      const fn = path.join(process.cwd(), prefix, filename);
+      const bytes = fs.readFileSync(fn);
+      const gzipped = zlib.gzipSync(bytes);
+      return gzipped.byteLength;
     })
     .reduce((s, b) => s + b, 0);
 
-    return { path: p, size };
+  return { path: p, size };
 });
 
-const output =
-`# Bundle Size
+const output = `# Bundle Size
 
 | Route | Size (gzipped) |
 | --- | --- |
-${pageSizes.map(({ path, size }) => `| \`${path}\` | ${formatBytes(size)} |`).join("\n")}
+${pageSizes
+  .map(({ path, size }) => `| \`${path}\` | ${formatBytes(size)} |`)
+  .join("\n")}
 
 <!-- GH BOT -->`;
 
 try {
-    fs.mkdirSync(outdir);
+  fs.mkdirSync(outdir);
 } catch (e) {
-    // may already exist
+  // may already exist
 }
+
 fs.writeFileSync(outfile, output);
+fs.writeFileSync(outfileJSON, JSON.stringify(pageSizes, null, 2));
