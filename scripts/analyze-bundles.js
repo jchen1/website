63a2cd103cdde98f1d8dc62d3114fab8a191158d
Jeff Chen <hello@jeff.yt>
2020-11-30 08:55:55 -0800

    bundle size comparison to master (#11)
    * yolo!
    
    * m
    
    * asdf
    
    * fixerup
    
    * Update compare-bundles.js
    
    * Update compare-bundles.js
    
    * more
    
    * revert me: test addition & removal
    
    * fix
    
    * Revert "revert me: test addition & removal"
    
    This reverts commit 3cf68652e47e43e14d6de51d9e33868a36665bd3.

--- scripts/analyze-bundles.js
+++ scripts/analyze-bundles.js
@@ -6,20 +6,7 @@ const bundle = require("../.next/build-manifest.json");
 
 const prefix = ".next";
 const outdir = path.join(process.cwd(), prefix, "analyze");
-const outfile = path.join(outdir, "bundle.txt");
 const outfileJSON = path.join(outdir, "bundle.json");
-
-function formatBytes(bytes, decimals = 2) {
-  if (bytes === 0) return "0 Bytes";
-
-  const k = 1024;
-  const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-}
 
 const pageSizes = Object.keys(bundle.pages).map(p => {
   const files = bundle.pages[p];
@@ -35,21 +22,10 @@ const pageSizes = Object.keys(bundle.pages).map(p => {
   return { path: p, size };
 });
 
-const output = `# Bundle Size
-
-| Route | Size (gzipped) |
-| --- | --- |
-${pageSizes
-  .map(({ path, size }) => `| \`${path}\` | ${formatBytes(size)} |`)
-  .join("\n")}
-
-<!-- GH BOT -->`;
-
 try {
   fs.mkdirSync(outdir);
 } catch (e) {
   // may already exist
 }
 
-fs.writeFileSync(outfile, output);
 fs.writeFileSync(outfileJSON, JSON.stringify(pageSizes, null, 2));
---
+++ scripts/compare-bundles.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const path = require("path");
+
+const currentBundle = require("../.next/analyze/bundle.json");
+const masterBundle = require("../.next/analyze/master/bundle/bundle.json");
+
+const prefix = ".next";
+const outdir = path.join(process.cwd(), prefix, "analyze");
+const outfile = path.join(outdir, "bundle-comparison.txt");
+
+function formatBytes(bytes, decimals = 2, signed = false) {
+  const sign = signed ? (bytes < 0 ? "-" : "+") : "";
+  if (bytes === 0) return `+0B`;
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${sign}${parseFloat(Math.abs(bytes / Math.pow(k, i)).toFixed(dm))}${
+    sizes[i]
+  }`;
+}
+
+const sizes = currentBundle
+  .map(({ path, size }) => {
+    const masterSize = masterBundle.find(x => x.path === path);
+    const diffStr = masterSize ? formatBytes(size - masterSize.size) : "added";
+    return `| \`${path}\` | ${formatBytes(size)} (${diffStr}) |`;
+  })
+  .concat(
+    masterBundle
+      .filter(({ path }) => !currentBundle.find(x => x.path === path))
+      .map(({ path }) => `| \`${path}\` | removed |`)
+  )
+  .join("\n");
+
+const output = `# Bundle Size
+
+| Route | Size (gzipped) |
+| --- | --- |
+${sizes}
+
+<!-- GH BOT -->`;
+
+try {
+  fs.mkdirSync(outdir);
+} catch (e) {
+  // may already exist
+}
+
+fs.writeFileSync(outfile, output);
