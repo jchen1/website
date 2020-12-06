cd9c72ee5acd9d8317c00cc5dd04eb2c9dece009
Jeff Chen <jchen@ladderlife.com>
2020-12-05 19:27:10 -0800

    f it, next/image

--- components/BlogSnippet.jsx
+++ components/BlogSnippet.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Image from "components/Image";
+import Image from "next/image";
 
 import styles from "styles/components/Blog.module.scss";
 
