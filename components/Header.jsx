f3a01aa56600802ce4c3c6e194d92df3dadd2338
Jeff Chen <jchen@ladderlife.com>
2020-12-30 07:55:01 -0800

    simplify header

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,6 +15,50 @@ export default function Header() {
   // prevent "auto-prefetch based on viewport... warning"
   const prefetch = router.pathname === "/" ? false : undefined;
   return (
+    <header className={styles.header}>
+      <nav className={styles.container}>
+        <Link href="/" prefetch={prefetch}>
+          <a className={styles.siteAvatar} aria-label="Home">
+            <Image
+              src="/images/headshot-1200.jpg"
+              alt="Profile Picture"
+              height={PROFILE_SIZE}
+              width={PROFILE_SIZE}
+              priority={true}
+              layout="responsive"
+            />
+          </a>
+        </Link>
+        <div className={styles.links}>
+          <div className={styles.siteInfo}>
+            <h1 className={styles.siteName}>
+              <Link href="/" prefetch={prefetch}>
+                <a>{SITE_TITLE}</a>
+              </Link>
+            </h1>
+            <p className={styles.description}>{SITE_DESCRIPTION}</p>
+          </div>
+          <div className={styles.spacer} />
+          <Link href="/about">
+            <a className={styles.link}>About</a>
+          </Link>
+          <Link href="/projects">
+            <a className={styles.link}>Projects</a>
+          </Link>
+          <Link href="/archive">
+            <a className={styles.link}>Archive</a>
+          </Link>
+          <Twitter
+            eventAction="header-cta-click"
+            size={25}
+            className={styles.link}
+          />
+        </div>
+      </nav>
+    </header>
+  );
+
+  return (
     <header className={styles.masthead}>
       <div className={styles.container}>
         <div className={styles.headerLeft}>
